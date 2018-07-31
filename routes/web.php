b438c790ff6372daaa4f506fa06dc578d86298e6
arriturnguyen <mrdatmoon@gmail.com>
2018-07-31 19:44:16 +0700

    Config database

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
+// Them moi va chinh sua cac URI hop le o file nay.
+
 Route::get('/', function () {
+	echo "TEST";exit;
+    return view('welcome');
+});
+
+Route::get('/test_URI_2', function () {
+	echo "TEST URI 2";exit;
     return view('welcome');
 });
